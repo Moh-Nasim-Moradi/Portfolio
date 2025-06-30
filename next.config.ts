0b304c4e9ca46bcf1a9681027f49b1f9f2930023
Sadeq Mozaffari <sadeqmozaffari@gmail.com>
2025-06-30 16:35:42 +0430

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
+/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['localhost'],
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? '/portfolio/' : '',
-  basePath: isProd ? '/portfolio' : '',
   trailingSlash: true,
   output: 'export',
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
+  images: {
+    unoptimized: true, // چون next/image در حالت export پشتیبانی کامل ندارد
+  },
 };
 
 export default nextConfig;
